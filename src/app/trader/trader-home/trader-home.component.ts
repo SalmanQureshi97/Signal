7563614d57df014cd5fca9e67c32fa363bebe128
Salman Qureshi <45896078+RundownCactus@users.noreply.github.com>
2022-01-14 01:51:39 +0500

    Trader View setup

--- src/app/trader/trader-home/trader-home.component.ts
+++ src/app/trader/trader-home/trader-home.component.ts
@@ -6,9 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trader-home.component.scss'],
 })
 export class TraderHomeComponent implements OnInit {
-
-  constructor() { }
+  random;
+  items = [
+    {
+      img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+      latestPrice: 76865,
+      mCap: '23B',
+      title: 'Ethereum',
+    },
+    {
+      img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+      latestPrice: 76865,
+      mCap: '23B',
+      title: 'Bitcoin',
+    },
+    {
+      img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+      latestPrice: 76865,
+      mCap: '23B',
+      title: 'Cardano',
+    },
+    {
+      img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y',
+      latestPrice: 76865,
+      mCap: '23B',
+      title: 'Polkadot',
+    },
+  ];
+  constructor() {}
 
   ngOnInit() {}
-
 }
--- src/app/trader/trader.module.ts
+++ src/app/trader/trader.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TraderRoutingModule } from './trader-routing.module';
 import { TraderHomeComponent } from './trader-home/trader-home.component';
+import { GridListComponent } from '../shared/grid-list/grid-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [TraderHomeComponent],
-  imports: [CommonModule, TraderRoutingModule],
+  imports: [CommonModule, TraderRoutingModule, SharedModule],
 })
 export class TraderModule {}
